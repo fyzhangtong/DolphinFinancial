5a5e75ba4464dafac46a48af89c132a7ff48463b
FDXDZ <zt@fdxdzdemacbook-pro.local>
2018-03-15 18:02:53 +0800

    2018-03-15 18:02 创建工程

---
+++ DolphinFinancial/DolphinFinancial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DolphinFinancial
+//
+//  Created by FDXDZ on 2018/3/15.
+//  Copyright © 2018年 zhantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
