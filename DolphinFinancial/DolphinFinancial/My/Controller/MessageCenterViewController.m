f94457409c3104786f811d6f3d024270477b02fd
FDXDZ <zt@fdxdzdemacbook-pro.local>
2018-03-27 12:14:02 +0800

    2018-03-27 12:13 完成消息中心页UI。

--- DolphinFinancial/DolphinFinancial/My/Controller/MessageCenterViewController.m
+++ DolphinFinancial/DolphinFinancial/My/Controller/MessageCenterViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "MessageCenterViewController.h"
+#import "MessageCenterTableViewCell.h"
+#import "BaseTableView.h"
 
-@interface MessageCenterViewController ()
+@interface MessageCenterViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) BaseTableView *tableView;
 
 @end
 
@@ -30,6 +34,62 @@
 {
     [self setCenterTitle:@"消息中心"];
     [self addLeftBackButton];
+    
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.ownNavigationBar.mas_bottom);
+        make.bottom.mas_equalTo(self.view.mas_bottom).mas_offset(-49);
+        make.left.right.mas_equalTo(self.view);
+    }];
+}
+
+- (BaseTableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[BaseTableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        [MessageCenterTableViewCell registerCellTableView:_tableView];
+        _tableView.delegate  = self;
+        _tableView.dataSource = self;
+        _tableView.tableFooterView = [UIView new];
+        [_tableView setBackgroundColor:DFColorWithHexString(@"#F8F8F8")];
+        [_tableView setSeparatorColor:DFColorWithHexString(@"#F8F8F8")];
+        if ([_tableView respondsToSelector:@selector(setSeparatorInset:)]) {
+            [_tableView setSeparatorInset:UIEdgeInsetsZero];
+        }
+        if ([_tableView respondsToSelector:@selector(setLayoutMargins:)]) {
+            [_tableView setLayoutMargins:UIEdgeInsetsZero];
+        }
+    }
+    return _tableView;
+}
+#pragma mark - tableViewDataSource
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [MessageCenterTableViewCell cellHeight];
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MessageCenterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[MessageCenterTableViewCell reuseIdentifier]];
+    
+    return cell;
+}
+
+#pragma mark - tableViewDelegate
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
+    }
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
---
+++ DolphinFinancial/DolphinFinancial/My/View/MessageCenterTableViewCell.m
@@ -0,0 +1,109 @@
+//
+//  MessageCenterTableViewCell.m
+//  DolphinFinancial
+//
+//  Created by FDXDZ on 2018/3/27.
+//  Copyright © 2018年 zhantong. All rights reserved.
+//
+
+#import "MessageCenterTableViewCell.h"
+
+@interface MessageCenterTableViewCell()
+
+@property (nonatomic, strong) UILabel *tagLabel;
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *dateLabel;
+@property (nonatomic, strong) UIImageView *rightArrowImageView;
+
+@end
+
+@implementation MessageCenterTableViewCell
+
++ (CGFloat)cellHeight
+{
+    return 60.f;
+}
+
+- (void)makeView
+{
+    
+    [self.contentView addSubview:self.tagLabel];
+    [_tagLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.contentView.mas_top).mas_offset(13);
+        make.left.mas_equalTo(self.contentView.mas_left).mas_offset(16);
+        make.width.mas_equalTo(34);
+        make.height.mas_equalTo(22);
+    }];
+    
+    [self.contentView addSubview:self.titleLabel];
+    [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(_tagLabel.mas_right).mas_offset(15);
+        make.top.mas_equalTo(_tagLabel.mas_top);
+        make.right.mas_equalTo(self.contentView.mas_right).mas_offset(-80);
+    }];
+    
+    [self.contentView addSubview:self.dateLabel];
+    [_dateLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(_titleLabel.mas_left);
+        make.bottom.mas_equalTo(self.contentView.mas_bottom);
+        make.right.mas_equalTo(_titleLabel.mas_right);
+        make.height.mas_equalTo(23);
+    }];
+    
+    [self.contentView addSubview:self.rightArrowImageView];
+    [self.rightArrowImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.mas_equalTo(self.contentView.mas_centerY);
+        make.right.mas_equalTo(self.contentView.mas_right).mas_offset(-8);
+        make.width.height.mas_equalTo(24);
+    }];
+}
+
+#pragma mark - getter
+
+- (UILabel *)tagLabel
+{
+    if (!_tagLabel) {
+        _tagLabel = [UILabel new];
+        _tagLabel.textColor = DFColorWithHexString(@"#101010");
+        _tagLabel.font = [UIFont systemFontOfSize:10];
+        _tagLabel.textAlignment = NSTextAlignmentCenter;
+        _tagLabel.layer.masksToBounds = YES;
+        _tagLabel.layer.cornerRadius = 4;
+        _tagLabel.layer.borderWidth = 1;
+        _tagLabel.layer.borderColor = DFColorWithHexString(@"#BBBBBB").CGColor;
+        _tagLabel.text = @"公告";
+    }
+    return _tagLabel;
+}
+- (UILabel *)titleLabel
+{
+    if (!_titleLabel) {
+        _titleLabel = [UILabel new];
+        _titleLabel.textColor = DFColorWithHexString(@"#101010");
+        _titleLabel.font = [UIFont systemFontOfSize:12];
+        _titleLabel.textAlignment = NSTextAlignmentLeft;
+        _titleLabel.text = @"关于海豚理财正式运营通知";
+    }
+    return _titleLabel;
+}
+- (UILabel *)dateLabel
+{
+    if (!_dateLabel) {
+        _dateLabel = [UILabel new];
+        _dateLabel.textColor = DFColorWithHexString(@"#969696");
+        _dateLabel.font = [UIFont systemFontOfSize:10];
+        _dateLabel.textAlignment = NSTextAlignmentLeft;
+        _dateLabel.text = @"2018-02-28 11:19:01";
+    }
+    return _dateLabel;
+}
+
+- (UIImageView *)rightArrowImageView
+{
+    if (!_rightArrowImageView) {
+        _rightArrowImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"rightArrow"]];
+    }
+    return _rightArrowImageView;
+}
+
+@end
